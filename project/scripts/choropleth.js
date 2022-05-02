fbf0cee4a7b7f580efeec46a658abc7eaaab9a94
duyanhvuong2001 <102603197@student.swin.edu.au>
2022-05-02 12:02:26 +1000

    update

--- project/scripts/choropleth.js
+++ project/scripts/choropleth.js
@@ -7,22 +7,90 @@ function init() {
                         .translate([w/2,h/2])
                         .scale(600);//expand the map
 
-    var path = d3.geoPath().projection(projection);
+    var path = d3.geoPath().projection(projection);//map projection 
 
-    var svg = d3.select("#australia_choropleth")
+    var svg = d3.select("#australia_choropleth")//set up the svg
                 .append("svg")
                 .attr("width",w)
                 .attr("height",h)
                 .attr("fill","teal");
-    d3.json("json/auGeoJson.json",function(json){//load in geo-json data 
+    //color scheme
+    var color = d3.scaleQuantize()
+                    .range(["#f2f0f7",
+                            "#9e9ac8",
+                            "#54278f"]);
 
-        svg.selectAll("path") 
-            .data(json.features)//create one path element for each features
+    // d3.json("json/auGeoJson.json",function(json){//load in geo-json data 
+
+    //     svg.selectAll("path") 
+    //         .data(json.features)//create one path element for each features
+    //         .enter()
+    //         .append("path")
+    //         .attr("d",path)
+           
+
+    //         .style("stroke","black")//stroke color
+    //         .style("stroke-width",0.8)
+    //         .style("opacity", 0.8)//opacity
+    //         .on("mouseover", mouseOver)//mouse over trigger
+    //         .on("mouseleave", mouseLeave)//mouse out trigger
+    //         ;
+    // });
+
+    d3.csv("data/auConsumptionPerState19-20.csv", function(data){
+        //map the color range based on loaded data
+        color.domain(
+            [
+                d3.min(data,function(d){return d.PJ;}),
+                d3.max(data,function(d){return d.PJ;})
+            ]
+        );
+        
+        d3.json("json/auGeoJson.json", function(json) {
+            //merge unemployment data and GeoJSON
+            //Loop through once for each unemployment value
+            
+            for(var i=0; i < data.length; i++){
+                //town name
+                var dataState = data[i].State;
+                
+                //data value, convert from string to float
+                var dataVal = parseFloat(data[i].PJ);
+                
+                //find corresponding town inside GeoJSON
+                for (var j = 0; j < json.features.length; j++){
+                    var jsonState = json.features[j].properties.STATE_CODE;
+                    
+                    if(dataState == jsonState) {
+                        //Copy the data value into the JSON
+                        json.features[j].properties.value = dataVal;
+                        
+                        //stop looping through JSON;
+                        break;
+                    }
+                }
+            };
+            //GeoJSON 
+            
+            svg.selectAll("path")
+            .data(json.features)
             .enter()
             .append("path")
             .attr("d",path)
             .attr("class",function(d){//give each feature a class
                 return "state";
+            })
+            .style("fill",function(d){
+                //get data value
+                var value = d.properties.value;
+                if(value) {
+                    //value exists?
+                    return color(value);
+                }
+                else {
+                    //if value is undefined, return
+                    return "#ccc";
+                }
             })
 
             .style("stroke","black")//stroke color
@@ -31,8 +99,8 @@ function init() {
             .on("mouseover", mouseOver)//mouse over trigger
             .on("mouseleave", mouseLeave)//mouse out trigger
             ;
+        });
     });
-
 
     //---------------------------FUNCTIONS------------------------
 
