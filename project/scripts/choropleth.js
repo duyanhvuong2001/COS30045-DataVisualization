41dc08bb1f0f8b2c877c8bda95981c87393b14f1
duyanhvuong2001 <102603197@student.swin.edu.au>
2022-05-02 11:44:25 +1000

    update

--- project/scripts/choropleth.js
+++ project/scripts/choropleth.js
@@ -14,15 +14,49 @@ function init() {
                 .attr("width",w)
                 .attr("height",h)
                 .attr("fill","teal");
-    
-    d3.json("json/auGeoJson.json").then(function(json){//load in geo-json data 
+    d3.json("json/auGeoJson.json",function(json){//load in geo-json data 
 
         svg.selectAll("path") 
             .data(json.features)//create one path element for each features
             .enter()
             .append("path")
-            .attr("d",path);
+            .attr("d",path)
+            .attr("class",function(d){//give each feature a class
+                return "state";
+            })
+
+            .style("stroke","black")//stroke color
+            .style("stroke-width",0.8)
+            .style("opacity", 0.8)//opacity
+            .on("mouseover", mouseOver)//mouse over trigger
+            .on("mouseleave", mouseLeave)//mouse out trigger
+            ;
     });
+
+    //mouseOver function
+    var mouseOver = function(d) {
+        d3.selectAll(".state")
+            .transition()
+            .duration(200)
+            .style("opacity", .5);//fade other states
+
+        d3.select(this) //highlight this state
+            .transition()
+            .duration(200)
+            .style("opacity", 1)
+            .style("stroke", "red")
+    }
+
+    //mouseOut function
+    var mouseLeave = function(d) {
+        d3.selectAll(".state")
+            .transition()
+            .duration(200)
+            .style("stroke","black")//stroke color
+            .style("stroke-width",0.8)
+            .style("opacity", 0.8)//opacity
+    }
+
 }
 
 window.onload =init;
