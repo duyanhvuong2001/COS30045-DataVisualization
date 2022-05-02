3abe02a2481fd8ac27d2ddb39ac0a71cce636249
duyanhvuong2001 <102603197@student.swin.edu.au>
2022-05-02 19:33:42 +1000

    update

--- project/scripts/choropleth.js
+++ project/scripts/choropleth.js
@@ -1,7 +1,7 @@
 function init() {
     var w = 700;
     var h = 500;
-    
+
     var projection = d3.geoMercator() //projection of the map
                         .center([135, -26])
                         .translate([w/2,h/2])
@@ -14,11 +14,6 @@ function init() {
                 .attr("width",w)
                 .attr("height",h)
                 .attr("fill","teal");
-    //color scheme
-    // var color = d3.scaleQuantize()
-    //                 .range(["#f2f0f7",
-    //                         "#9e9ac8",
-    //                         "#54278f"]);
 
     var color = d3.scaleQuantize()
                     .range(['#543005','#8c510a','#bf812d','#dfc27d','#f6e8c3','#c7eae5','#80cdc1','#35978f','#01665e','#003c30']);
@@ -32,6 +27,8 @@ function init() {
             ]
         );
         
+        
+
         d3.json("json/auGeoJson.json", function(json) {
             //merge unemployment data and GeoJSON
             //Loop through once for each unemployment value
@@ -49,8 +46,18 @@ function init() {
                     
                     if(dataState == jsonState) {
                         //Copy the data value into the JSON
-                        json.features[j].properties.value = dataVal;
-                        
+                        var properties = json.features[j].properties;
+                        properties.value = dataVal;
+                        properties.black_coal = data[i].black_coal;
+                        properties.brown_coal = data[i].brown_coal;
+                        properties.natural_gas = data[i].natural_gas;
+                        properties.oil_products = data[i].oil_products;
+                        properties.bagasse_wood = data[i].bagasse_wood;
+                        properties.biogas = data[i].biogas;
+                        properties.wind = data[i].wind;
+                        properties.hydro = data[i].hydro;
+                        properties.large_scale_solar_PV = data[i].large_scale_solar_PV;
+                        properties.small_scale_solar_PV = data[i].small_scale_solar_PV;
                         //stop looping through JSON;
                         break;
                     }
@@ -82,8 +89,8 @@ function init() {
             .style("stroke","black")//stroke color
             .style("stroke-width",0.8)
             .style("opacity", 0.8)//opacity
-            .on("mouseover", mouseOver)//mouse over trigger
-            .on("mouseleave", mouseLeave)//mouse out trigger
+            .on("mouseover", mouseOverMap)//mouse over trigger
+            .on("mouseleave", mouseLeaveMap)//mouse out trigger
             ;
         });
     });
@@ -91,7 +98,8 @@ function init() {
     //---------------------------INTERACTIVITY FUNCTIONS------------------------
 
     //mouseOver function
-    var mouseOver = function(d) {
+    var mouseOverMap = function(d) {
+        //----------FIRST HIGHLIGHT THE STATE--------------------------------
         d3.selectAll(".state")
             .transition()
             .duration(200)
@@ -105,10 +113,41 @@ function init() {
             .duration(200)
             .style("opacity", 1)
             .style("stroke", "red")
+
+        //--------THEN SHOW ANOTHER DATA VISUALIZATION-------
+
+        //---A CHART--------------------------------
+
+        d3.selectAll("#detailed_info > *").remove();//clear the svg
+
+        var w = 300;
+        var h = 300;
+        var horizontalPadding = 10;
+        var verticalPadding = 30;
+
+        var dataset = [
+                        {category: "Black Coal", value: d.properties.black_coal},
+                        {category: "Brown Coal", value: d.properties.brown_coal},
+                        {category: "Natural Gas", value: d.properties.natural_gas},
+                        {category: "Oil Products", value: d.properties.oil_products},
+                        {category: "Bagasse/Wood ", value: d.properties.bagasse_wood},
+                        {category: "Biogas", value: d.properties.biogas},
+                        {category: "Wind", value: d.properties.wind},
+                        {category: "Hydro", value: d.properties.hydro},
+                        {category: "Large Scale Solar PV", value: d.properties.large_scale_solar_PV},
+                        {category: "Small Scale Solar PV", value: d.properties.small_scale_solar_PV}
+                        ];
+        document.getElementById("state_name").innerHTML = d.properties.STATE_NAME;//change title to state state_name
+
+        var svg = d3.select("#detailed_info")//create the svg
+        .append("svg")
+        .attr("width",w)
+        .attr("height",h);
+
     }
 
     //mouseOut function
-    var mouseLeave = function(d) {
+    var mouseLeaveMap = function(d) {
         d3.selectAll(".state")
             .transition()
             .duration(200)
